480245c0eee6610c5b4a95453ddc3bcdfd9e18e4
bmberger <brianaberger@google.com>
2024-11-12 12:11:29 -0800

    Add sudo to test permission theory

--- pkg/build/fuchsia.go
+++ pkg/build/fuchsia.go
@@ -101,7 +101,7 @@ func (fu fuchsia) clean(params Params) error {
 }
 
 func runSandboxed(timeout time.Duration, dir, command string, arg ...string) ([]byte, error) {
-	cmd := osutil.Command(command, arg...)
+	cmd := osutil.Command("sudo " + command, arg...)
 	fmt.Println("cmd:", cmd)
 	cmd.Dir = dir
 	if err := osutil.Sandbox(cmd, true, false); err != nil {
